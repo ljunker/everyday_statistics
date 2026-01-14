7c3fd24b445bf33c01ce0b16f9b0435ed8856c76
Lars Junker <larsjunker92@gmail.com>
2026-01-14 11:20:25 +0100

    feat(docker) [skip ci]: enhance `docker compose` command with improved service re-creation flags
    - Added `--force-recreate` and `--remove-orphans` flags to ensure services are fully rebuilt and unused containers are cleaned up during deployment.

--- upgrade.sh
+++ upgrade.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker compose up --build -d
+docker compose up --build --force-recreate --remove-orphans -d
 # For SQLite, the database will be created automatically if it doesn't exist
 # We use flask shell to ensure the tables are created if starting fresh
 docker compose exec web python3 -c "
